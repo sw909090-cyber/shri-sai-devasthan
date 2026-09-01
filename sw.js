f5e6cfbef5aca71136f7c65611185df9a6d2f4fa
sw909090-cyber <sw909090@gmail.com>
2026-09-02 02:20:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='sai-devasthan-v7';
+// whatsapp-fix-2026-09-02
+const CACHE='sai-devasthan-v8';
 const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
