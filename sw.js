01b966bbc24b217c85e7bc4a14ecee7df4c3c8d9
sw909090-cyber <sw909090@gmail.com>
2026-09-02 02:28:38 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,3 +4,5 @@ const CORE=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{ if(e.request.method==='GET') e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;}).catch(()=>caches.match('./index.html'))));});
+
+// Gallery organization + slideshow cache update v8
